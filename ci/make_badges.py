75c75f72acfa2acfa16782d5db42033767b1e1dc
Ingo Breßler <dev@ingobressler.net>
2023-03-23 15:29:38 +0100

    chore(GitHubAction): fix badge colors

--- ci/make_badges.py
+++ ci/make_badges.py
@@ -5,6 +5,10 @@ from pathlib import Path
 # generate some badges with metadata of failure by default
 # (do be overwritten on success)
 
+# see https://github.com/badges/shields/blob/master/badge-maker/lib/color.js
+colorGood = "success"
+colorBad = "critical"
+
 try:
     cmd = sys.argv[1]
     assert cmd in ("create", "update")
@@ -21,14 +25,16 @@ if cmd == "create":
     badgeDir = distPath / "badges"
     badgeDir.mkdir(parents=True, exist_ok=True)
     for topic in ("Docs", "Build", "Tests"):
+        data = dict(schemaVersion=1, label=topic, message="Fail", color=colorBad, style="plastic")
         with open(badgeDir / (topic.lower() + ".json"), "w") as fh:
-            fh.write('{"schemaVersion":1,"label":"' + topic + '","message":"Fail","color":"red"}')
+            json.dump(data, fh)
+
 elif cmd == "update":
     assert distPath.is_file(), "Provided file path does not exist!"
     data = None
     with open(distPath) as fh:
         data = json.load(fh)
     data["message"] = "Good"
-    data["color"] = "green"
+    data["color"] = colorGood
     with open(distPath, "w") as fh:
         json.dump(data, fh)
