c04b96a20aac76fdf224b9d4bd45794eb67e8744
toqduj <brian.pauw@bam.de>
2025-07-25 09:19:36 +0200

    preparing for units.

--- src/mcsas3/__init__.py
+++ src/mcsas3/__init__.py
@@ -7,4 +7,13 @@ Refactored McSAS implementation
 
 __version__ = "1.0.5"
 
+from pint import UnitRegistry, set_application_registry
+
+ureg = UnitRegistry(system="SI")
+Q_ = ureg.Quantity
+# recommended for pickling and unpickling:
+set_application_registry(ureg)
+ureg.formatter.default_format = "~P"
+ureg.setup_matplotlib(True)
+
 # vim: set ts=4 sts=4 sw=4 tw=0:
